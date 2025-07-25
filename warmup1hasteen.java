87bf8fa39c17ee9612c2348cb09deef1d20a1f6a
Abdulmalik <abdulmalikalnodhairi@gmail.com>
2025-07-25 19:24:36 +0400

    New solutions added

---
+++ warmup1hasteen.java
@@ -0,0 +1,5 @@
+public class warmup1hasteen {
+    public boolean hasTeen(int a, int b, int c) {
+        return(a>12 && a<20 || b>12 && b<20 || c>12 && c<20);
+    }
+}
--- warmup1or35.java
+++ warmup1or35.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class warmup1or35 {
+    public boolean or35(int n) {
+        return(n%3==0|| n%5==0);
+    }
 }
