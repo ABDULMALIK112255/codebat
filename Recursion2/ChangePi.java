92c6e47d6e7f33449cf9a275cf22c6a8b4b41a30
Abdulmalik <abdulmalikalnodhairi@gmail.com>
2025-08-03 11:00:53 +0400

    Finished Recrusion1 and 2

---
+++ Recursion2/ChangePi.java
@@ -0,0 +1,8 @@
+public String changePi(String str) {
+    if (str.length() < 2) return str;
+    if (str.startsWith("pi")) {
+        return "3.14" + changePi(str.substring(2));
+    } else {
+        return str.charAt(0) + changePi(str.substring(1));
+    }
+}
